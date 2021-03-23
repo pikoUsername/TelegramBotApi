0bec11e1dd9dbb6caad6d2d10891d35cb2c9247f
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-03-23 11:03:21 +0600

    bot update
    fix compile error, using decoding, and MakeRequest is dont make a compile error, finally

--- iternal/telegram/bot.go
+++ iternal/telegram/bot.go
@@ -15,7 +15,7 @@ import (
 type Bot struct {
 	Token string
 
-	Me types.User `json:"-"`
+	Me *types.User `json:"-"`
 }
 
 // TelegramURL ...
@@ -37,17 +37,25 @@ func NewBot(token string, checkToken bool) *Bot {
 // MakeRequest to telegram servers
 // and result parses
 func MakeRequest(Method string, Token string) (*types.TelegramResponse, error) {
+	// Bad Code, but working, huh
 	resp, err := http.Post(TelegramURL+"/"+Token+"/"+Method, "application/json", &strings.Reader{})
 	if err != nil {
 		return nil, errors.New("Error on sending request")
 	}
 	defer resp.Body.Close()
+
+	var tgresp types.TelegramResponse
 	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(resp)
+	err = dec.Decode(&tgresp)
 	if err != nil {
-		return nil, errors.New("Error on json decode")
+		return nil, err
 	}
-	return resp, nil
+
+	if err := utils.CheckResult(Method, tgresp.ErrorCode); err != nil {
+		return &tgresp, err
+	}
+
+	return &tgresp, nil
 }
 
 // GetMe ...
@@ -57,10 +65,6 @@ func (bot *Bot) GetMe() *types.User {
 
 // SendMessage ...
 func (b *Bot) SendMessage(text string) (*types.Message, error) {
-	resp, err := MakeRequest("SendMessage", b.Token)
-	if err != nil {
-		return &types.Message{}, nil
-	}
-	json.Unmarshal(resp.Body)
+	// resp, err := MakeRequest("SendMessage", b.Token)
 	return &types.Message{}, nil
 }
