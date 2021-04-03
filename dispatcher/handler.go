20c83cfc1efcc478740a8d507af9cc691bb1c0af
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-04-03 15:39:42 +0600

    added method for HandlerObj

--- dispatcher/handler.go
+++ dispatcher/handler.go
@@ -1,6 +1,22 @@
 package dispatcher
 
+import "github.com/pikoUsername/tgp/bot"
+
 // HandlerObj uses for save Callback
 type HandlerObj struct {
-	Callabacks []interface{}
+	Callbacks   []*func(interface{}, *bot.Bot)
+	Middlewares []*func(interface{})
+}
+
+func (ho *HandlerObj) Register(f *func(interface{}, *bot.Bot)) {
+	ho.Callbacks = append(ho.Callbacks, f)
+}
+
+func (ho *HandlerObj) RegisterMiddleware(f *func(interface{})) {
+	ho.Middlewares = append(ho.Middlewares, f)
+}
+
+// Trigger is from aiogram framework
+func (ho *HandlerObj) Trigger(obj interface{}) {
+	return
 }
