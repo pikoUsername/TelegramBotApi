e76bd0c416308ecc7b9033e0314d1e4c3cc8797f
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-04-06 11:29:42 +0600

    Handler update

--- dispatcher/handler.go
+++ dispatcher/handler.go
@@ -4,21 +4,30 @@ import "github.com/pikoUsername/tgp/bot"
 
 // HandlerObj uses for save Callback
 type HandlerObj struct {
-	Callbacks   []*func(interface{}, *bot.Bot)
-	Middlewares []*func(interface{})
+	Callbacks   []*func(interface{}, bot.Bot)
+	Middlewares []func(interface{}, *func(interface{}, bot.Bot)) *func(interface{}, bot.Bot)
 }
 
-func (ho *HandlerObj) Register(f *func(interface{}, *bot.Bot)) {
+// Register, append to Callbacks, e.g handler functions
+func (ho *HandlerObj) Register(f *func(interface{}, bot.Bot)) {
 	ho.Callbacks = append(ho.Callbacks, f)
 }
 
-func (ho *HandlerObj) RegisterMiddleware(f *func(interface{})) {
+// RegisterMiddleware looks like a bad code
+// Register middlewares, except function which should return handler
+// e.g second parametr
+// for example, you want to register every user which writed to you bot
+// You can registerMiddleware for MessageHandler, not for all handlers
+// Or maybe want to make throttling middleware, just Registers middleware
+func (ho *HandlerObj) RegisterMiddleware(f func(interface{}, *func(interface{}, bot.Bot)) *func(interface{}, bot.Bot)) {
 	ho.Middlewares = append(ho.Middlewares, f)
 }
 
 // Trigger is from aiogram framework
-func (ho *HandlerObj) Trigger(obj interface{}) {
+func (ho *HandlerObj) Trigger(obj interface{}, bot bot.Bot) {
 	if ho.Middlewares != nil {
-
+		for _, f := range ho.Middlewares {
+			f(obj, nil) // stub
+		}
 	}
 }
