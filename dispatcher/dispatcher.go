fc2131dfb3d818469c2099dee4f72fdd3c9c9402
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-04-06 12:18:47 +0600

    Added StartPolling method

--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -2,6 +2,7 @@ package dispatcher
 
 import (
 	"github.com/pikoUsername/tgp/bot"
+	"github.com/pikoUsername/tgp/configs"
 	"github.com/pikoUsername/tgp/objects"
 )
 
@@ -18,14 +19,19 @@ import (
 type Dispatcher struct {
 	Bot *bot.Bot
 
-	MessageHandler *HandlerObj
+	// Handlers
+	MessageHandler       *HandlerObj
+	CallbackQueryHandler *HandlerObj
+
+	Polling bool
 }
 
 // NewDispathcer get a new Dispatcher
 // And with autoconfiguration, need to run once
 func NewDispatcher(bot *bot.Bot) (*Dispatcher, error) {
 	dp := &Dispatcher{
-		Bot: bot,
+		Bot:     bot,
+		Polling: false,
 	}
 	dp.Configure()
 	return dp, nil
@@ -38,24 +44,51 @@ func (dp *Dispatcher) Configure() {
 	dp.MessageHandler = &HandlerObj{}
 }
 
+func (dp *Dispatcher) ResetWebhook(check bool) error {
+	if check {
+		wi, err := dp.Bot.GetWebhookInfo()
+		if err != nil {
+			return err
+		}
+		if wi.URL == "" {
+			return nil
+		}
+	}
+	return dp.Bot.DeleteWebhook(&configs.DeleteWebhookConfig{})
+}
+
 // RegisterHandler excepts you pass to parametrs a your function
 // which have no returns
-func (dp *Dispatcher) RegisterHandler(callback func(interface{}, *bot.Bot)) {
-	dp.MessageHandler.Register(&callback)
+func (dp *Dispatcher) RegisterHandler(callback *func(interface{}, bot.Bot)) {
+	dp.MessageHandler.Register(callback)
 }
 
 // ProcessUpdates havenot got any efficient
 // if you use webhook and long polling
-func (dp *Dispatcher) ProcessUpdates(update *objects.Update) {
-	return
+func (dp *Dispatcher) ProcessPollingUpdates(updates []objects.Update) error {
+	return nil // TODO
+}
+
+// ProcessUpdates using for process updates from any way
+func (dp *Dispatcher) ProcessUpdates(updates []*objects.Update) error {
+	return nil // TODO
 }
 
 // StartPolling check out to comming updates
 // If yes, Telegram Get to your bot a Update
 // Using GetUpdates method in Bot structure
-func (dp *Dispatcher) StartPolling(timeout int, limit int) error {
-	for {
-		break
+// GetUpdates config using for getUpdates method
+func (dp *Dispatcher) StartPolling(c *configs.GetUpdatesConfig) error {
+	dp.Polling = true
+	for dp.Polling {
+		// TODO: timeout
+		updates, err := dp.Bot.GetUpdates(c)
+		if err != nil {
+			return err
+		}
+		if updates != nil {
+			dp.ProcessPollingUpdates(updates)
+		}
 	}
 	return nil
 }
