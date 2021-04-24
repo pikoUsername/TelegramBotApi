3482f32a328d222f20d9a7934e64202617732454
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-04-24 21:43:01 +0600

    removed Messageable structure

--- objects/base.go
+++ objects/base.go
@@ -4,16 +4,6 @@ import (
 	"encoding/json"
 )
 
-// MessageAble uses for Send function
-// I love this, when you can instead just interface{}
-// Use special interface, and your function can made
-// This easily
-type MessageAble interface {
-	// Sorry but its impossible, make that without
-	// circular imports, so i used this ;(
-	Send(interface{}) *Message
-}
-
 // maybe most important peace of code
 type TelegramResponse struct {
 	Ok          bool                `json:"ok"`
