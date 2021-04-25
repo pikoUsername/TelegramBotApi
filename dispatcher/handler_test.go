da55c72d397dcc559eaf3ab15110f8c8ed0b1f08
pikoUsername <galymzhan.amantaj@gmail.com>
2021-04-25 17:01:55 +0300

    Added comments to test
    why XD

--- dispatcher/handler_test.go
+++ dispatcher/handler_test.go
@@ -34,6 +34,14 @@ func TestMiddlwareRegister(t *testing.T) {
 	}
 	dp.MessageHandler.RegisterMiddleware(func(u *objects.Update, ht dispatcher.HandlerType) {
 		u.Message.Text = "жил был, один фанат исекая. А на завтра его сбила машина, КОНЕЦ(а он ждал, и ждал в бесонечной темноте, пока его личность не растворилась навсегда)!"
+		// Or if you want register user in Database:
+		// go storage.RegisterUser(&u.Message.From)
+		// or ACL:
+		// usr := storage.GetUser(&u.Message.From.ID)
+		// if usr == nil { SendError("Not allowed!!!")}
 		ht(*u)
+		// Here you can use any metric
+		// go metrics.CreateMetricBaseAtUser(&u.Message.From)
+		// or anything you can image! as always ;)
 	})
 }
