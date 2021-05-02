6d709b8530d014ba3ca85db7886f335ea0b58308
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-05-02 14:11:05 +0600

    refactor code
    moved Permissions to helpers, and ParseModes too

--- utils/helpers.go
+++ utils/helpers.go
@@ -1,5 +1,30 @@
 package utils
 
+// Will be a other stuff, but useful stuff
+
+// actions for telegram bot api
+const (
+	TYPING            = "TYPING"            // typing
+	UPLOAD_PHOTO      = "UPLOAD_PHOTO"      // upload_photo
+	RECORD_VIDEO      = "RECORD_VIDEO"      // record_video
+	UPLOAD_VIDEO      = "UPLOAD_VIDEO"      // upload_video
+	RECORD_AUDIO      = "RECORD_AUDIO"      // record_audio
+	UPLOAD_AUDIO      = "UPLOAD_AUDIO"      // upload_audio
+	RECORD_VOICE      = "RECORD_VOICE"      // record_voice
+	UPLOAD_VOICE      = "UPLOAD_VOICE"      // upload_voice
+	UPLOAD_DOCUMENT   = "UPLOAD_DOCUMENT"   // upload_docuemnt
+	FIND_LOCATION     = "FIND_LOCATION"     // find_location
+	RECORD_VIDEO_NOTE = "RECORD_VIDEO_NOTE" // record_video_note
+	UPLOAD_VIDEO_NOTE = "UPLOAD_VIDEO_NOTE" // upload_video_note
+)
+
+// ParseModes
+const (
+	ModeMarkdown   = "Markdown"
+	ModeMarkdownV2 = "MarkdownV2"
+	ModeHTML       = "HTML"
+)
+
 // Not Reporesenting any object
 type Permissions struct {
 	CanBeEdited           bool `json:"can_be_edited"`
--- utils/parsemodes.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-// ParseModes
-const (
-	ModeMarkdown   = "Markdown"
-	ModeMarkdownV2 = "MarkdownV2"
-	ModeHTML       = "HTML"
-)
