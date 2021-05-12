5cfcf3f21ac0ea2e0ccf3c7f9b281bdcf710501b
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-05-12 23:54:03 +0600

    added poll support, and Shipping quries

--- objects/poll.go
+++ objects/poll.go
@@ -1,8 +1,30 @@
 package objects
 
+import "time"
+
 type Poll struct {
-	Question string `json:"question"`
+	ID                    int64            `json:"id"`
+	Question              string           `json:"question"`
+	Options               []*PollOption    `json:"options"`
+	TotalVoterCount       int              `json:"total_voter_count"`
+	IsClosed              bool             `json:"is_closed"`
+	IsAnonymoust          bool             `json:"is_anonymous"`
+	Type                  string           `json:"type"`
+	AllowsMultipleAnswers bool             `json:"allows_multiple_answers"`
+	CorrectOptionId       int64            `json:"correct_option_id"`
+	Explanation           string           `json:"explanation"`
+	ExplanationEntities   []*MessageEntity `json:"explanation_entities"`
+	OpenPeriod            time.Duration    `json:"open_period"`
+	CloseDate             time.Duration    `json:"close_date"`
 }
 
 type PollAnswer struct {
+	PollID    int64 `json:"poll_id"`
+	*User     `json:"user"`
+	OptionIDs []int64 `json:"option_ids"`
+}
+
+type PollOption struct {
+	Text       string `json:"text"`
+	VoterCount int    `json:"voter_count"`
 }
--- objects/shipping_query.go
+++ objects/shipping_query.go
@@ -1,4 +1,8 @@
 package objects
 
 type ShippingQuery struct {
+	ID               string `json:"id"`
+	From             *User  `json:"from"`
+	InvoicePayload   string `json:"invoice_payload"`
+	*ShippingAddress `json:"shipping_address"`
 }
