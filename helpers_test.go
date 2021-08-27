1788a410b872f1be9465b9e7b3c4fecdabeaa9b5
root <galymzhan.amantaj@gmail.com>
2021-08-27 16:29:29 +0600

    Added helpers file test

---
+++ helpers_test.go
@@ -0,0 +1,23 @@
+package tgp_test
+
+import (
+	"net/url"
+	"testing"
+)
+
+// go test -v -banchmem
+
+func TestUrlvaluesToMapString(t *testing.T) {
+	val := map[string]string{}
+	value_text := "exe"
+
+	v := url.Values{}
+
+	v.Add("kek", value_text)
+
+	urlValuesToMapString(v, val)
+
+	if v, ok := val["kek"]; !ok || v != value_text {
+		t.Fatal("value is not correct, converting value is not working")
+	}
+}
