1de5ab3e67895ddbea975d83b37ad10d34fa904f
pikoUsername <galymzhan.amantaj@gmail.com>
2022-02-08 16:52:01 +0600

    Context update - Fixed Next() method; Added Done() method;

--- context_test.go
+++ context_test.go
@@ -18,13 +18,26 @@ var (
 	}
 )
 
-func TestContextHandlerAbility(t *testing.T) {
+func GetContext(t *testing.T) *Context {
 	dp, err := GetDispatcher(false)
 	if err != nil {
 		t.Fatal(err)
 	}
-	ctx := dp.Context(fakeUpd)
-	t.Error(ctx)
+	return dp.Context(fakeUpd)
+}
+
+func TestContextNext(t *testing.T) {
+	ctx := GetContext(t)
+
+	var x = 0
+
+	h := NewHandlerType(func(c *Context) { x += 1 })
+	ctx.handlers = append(ctx.handlers, h)
+
+	ctx.Next()
+	if x != 1 {
+		t.Fatal("Handler didnt called")
+	}
 }
 
 func TestResetState(t *testing.T) {
